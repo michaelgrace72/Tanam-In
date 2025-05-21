136108ae0be4319f8886834f5c9d1b927e01c253
Abdurrahman Faiz <5025221058@student.its.ac.id>
2025-05-21 22:31:01 +0700

    add navbar component

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,14 @@
+<nav class="dark:bg-[#005247]">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+        <a href="https://flowbite.com" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Tanam.in</span>
+        </a>
+        <div class="flex items-center space-x-6 rtl:space-x-reverse">
+            <a href="/login"
+                class="text-sm text-gray-500 dark:text-white border border-[#FE8C00] dark:border-[#FE8C00] rounded px-4 py-1 hover:bg-gray-100 dark:hover:bg-[#004235] transition">Login</a>
+            <a href="/register"
+                class="text-sm text-black dark:text-black border border-[#FE8C00] rounded px-4 py-1 bg-[#FE8C00] hover:bg-blue-50 dark:hover:bg-[#F83600] transition">Register</a>
+        </div>
+    </div>
+</nav>
